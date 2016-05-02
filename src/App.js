46bcd5adb6b3c26628c16c8f81e2de5ec51c9b1f
meganmcgeee <meganpmcgee@gmail.com>
2016-05-02 11:20:15 -0700

    Created stateless app component in the app file.

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// Import react to so the app can use it
+import React from 'react';
+
+// Create stateless app component
+const App = () => <h1> Hello world</h1>
+
+// export app component
+export default App;
