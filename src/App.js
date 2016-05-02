209f3054413aeac6ab61ed34b18d9652fb19dca3
meganmcgeee <meganpmcgee@gmail.com>
2016-05-02 12:37:39 -0700

    Import react-router components and objects needed for project.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // Import react to so the app can use it
 import React from 'react';
+// Import react-router components and objects needed for project.
+import {Router, Route, Link, hashHistory } from 'react-router';
 
 // Create stateless app component
 const App = () => <h1> Hello world</h1>
