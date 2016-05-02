4de9ad8687d78162ff93958d03196fb264fec074
meganmcgeee <meganpmcgee@gmail.com>
2016-05-02 12:44:26 -0700

    Commented out placeholder app component. Added Home component.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import React from 'react';
 // Import react-router components and objects needed for project.
 import {Router, Route, Link, hashHistory } from 'react-router';
 
-// Create stateless app component
-const App = () => <h1> Hello world</h1>
+// Create stateless app component as placeholder
+// const App = () => <h1> Hello world</h1>
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+)
+
+
 
 // export app component
 export default App;
