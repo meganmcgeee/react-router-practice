aabe4978cd5ec259317c65b32c963141416e85c1
meganmcgeee <meganpmcgee@gmail.com>
2016-05-02 13:19:52 -0700

    Changed the value of the Router component's history property from hashHistory to browserHistory so that the urls look clean and don't have random numbers

--- src/App.js
+++ src/App.js
@@ -10,21 +10,21 @@ const Home = () => (
     <h1>Home</h1>
     <Links />
   </div>
-)
+);
 
 const About = () => (
   <div>
     <h1>About</h1>
     <Links />
   </div>
-)
+);
 
 const Contact = () => (
   <div>
     <h1>Contact</h1>
     <Links />
   </div>
-)
+);
 
 
 // Create links component to create navigation
@@ -40,7 +40,7 @@ const Links = () =>
 class App extends React.Component {
   render(){
     return (
-      <Router history={ hashHistory }>
+      <Router history={ browserHistory }>
         <Route path="/" component={Home}> </Route>
         <Route path="/about" component={About}> </Route>
         <Route path="/contact" component={Contact}> </Route>
